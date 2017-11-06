b85d8348cf9da03a3d223eff7a57fd7293f5a5fd
anhtranEH <anh.tran@employmenthero.com>
2017-11-06 16:35:31 +0700

    Add HTTP view

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
 source 'https://rubygems.org'
 
 gem "sinatra"
+gem "sinatra-reloader"
+gem 'pry'
+gem 'pry-byebug'
 gem "puma"
--- myapp.rb
+++ myapp.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
+require 'pry'
+require 'sinatra/reloader' if development?
 
-get '/' do
-  'Hello world!'
+class MyApp < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/view' do
+    erb :index
+  end
+
+  get '/' do
+    'Hello world!'
+  end
 end
